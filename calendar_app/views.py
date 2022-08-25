a2841598e6f8272145df0b4aa82dbcbe20917ac0
michaelrilan <micorilan1999@gmail.com>
2022-08-25 19:27:46 +0800

    fdsfdf

--- calendar_app/views.py
+++ calendar_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from datetime import date, timedelta
 import datetime
+
+def index(response):
+    return HttpResponse("<p>pukininam</p>")
