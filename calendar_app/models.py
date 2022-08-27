b2ae4594e5217faa736ad5882364c323bb4c599f
michaelrilan <micorilan1999@gmail.com>
2022-08-27 20:38:18 +0800

    finished

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -4,3 +4,4 @@ from django.db.models import aggregates
 class limit(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50) 
+    date_saved = models.CharField(max_length=100)
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -12,27 +12,33 @@ def index(request):
     form = mema_form()
     entry_limit = admin_limit.objects.values_list('entry_limit', flat=True).get(pk=1)
     date_blockzxc = admin_date_block.objects.values_list('date_block', flat=True).get(pk=1)
+
     date_time_obj = datetime.strptime(date_blockzxc, "%m/%d/%Y").date()
+    #month/day/year  hh:mm:ss
     date_time_obj_porma = date_time_obj.strftime("%m/%d/%Y")
+    #month/day/year
+
     petsa_ngayon = datetime.today().strftime("%m/%d/%Y")
-    cnt = limit.objects.all().count()
+    cnt = limit.objects.filter(date_saved = petsa_ngayon).count()
     print(petsa_ngayon)
     print(date_time_obj_porma)
     print(cnt)
     print(entry_limit)
     statee = ""
-    
-    if request.method == 'POST':
-        form = mema_form(request.POST)
-        if form.is_valid() and cnt<entry_limit and petsa_ngayon != date_time_obj_porma:
-            form.save()
-            messages.success(request, "Successfully Registered")
-        else:
-            statee = "exceeded"
-            messages.info(request, "Form_expired")
+    if cnt<entry_limit and petsa_ngayon != date_time_obj_porma:
+        if request.method == 'POST':
+            form = mema_form(request.POST)
+
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Successfully Registered")
+    else:
+        statee = "exceeded"
+        messages.info(request, "Form_expired")
     context = {'form': form,
-    "statee": statee
+    "statee": statee,
+    "petsa_ngayon": petsa_ngayon
     }
     return render(request, "index.html",context)
 
-#Prvrbs 16:4
+#Proverbs 16:4
